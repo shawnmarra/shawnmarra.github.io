899d9c307d40779f205e81c4c9f0c860eb3df595
Shawn Marra <shawn@truvisor.co>
2021-03-30 22:55:11 -0600

    more code

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,4 @@
-const words = ["QUIRE",	"TRELAWNEY,",	"Dr.",	"Livesey,",	"and",	"the",	"rest",	"of",	"these",	"gentlemen",	"having",	"asked",	"me",	"to",	"write",	"down",	"the",	"whole",	"particulars",	"about",	"Treasure",	"Island,",	"from",	"the",	"beginning",	"to",	"the",	"end,",	"keeping",	"nothing",	"back",	"but",	"the",	"bearings",	"of",	"the",	"island,",	"and",	"that",	"only",	"because",	"there",	"is",	"still",	"treasure",	"not",	"yet",	"lifted,",	"I",	"take",	"up",	"my",	"pen",	"in",	"the",	"year",	"of",	"grace",	"17__",	"and",	"go",	"back",	"to",	"the",	"time",	"when",	"my",	"father",	"kept",	"the",	"Admiral",	"Benbow",	"inn",	"and",	"the",	"brown",	"old",	"seaman",	"with",	"the",	"sabre",	"cut",	"first",	"took",	"up",	"his",	"lodging",	"under",	"our",	"roof.",
-"I",	"remember",	"him",	"as",	"if",	"it",	"were",	"yesterday,",	"as",	"he",	"came",	"plodding",	"to",	"the",	"inn",	"door,",	"his",	"sea-chest",	"following",	"behind",	"him",	"in",	"a",	"hand-barrow--a",	"tall,",	"strong,",	"heavy,",	"nut-brown",	"man,",	"his",	"tarry",	"pigtail",	"falling",	"over",	"the",	"shoulder",	"of",	"his",	"soiled",	"blue",	"coat,",	"his",	"hands",	"ragged",	"and",	"scarred,",	"with",	"black,",	"broken",	"nails,",	"and",	"the",	"sabre",	"cut",	"across",	"one",	"cheek,",	"a",	"dirty,",	"livid",	"white.",	"I",	"remember",	"him",	"looking",	"round",	"the",	"cove",	"and",	"whistling",	"to",	"himself",	"as",	"he",	"did",	"so,",	"and",	"then",	"breaking",	"out",	"in",	"that",	"old",	"sea-song",	"that",	"he",	"sang",	"so",	"often",	"afterwards:",
-]
+const words = ["> Hello"]
 
 let cursor = gsap.to('.cursor', {opacity: 0, ease: "power2.inOut", repeat: -1})
 
@@ -12,12 +10,10 @@ boxTL.to('.box', {duration: 1, width: "17vw", delay: 0.5, ease: "power4.inOut"})
     .to('.box', {duration: 2, autoAlpha: 0.5, yoyo: true, repeat: -1, ease:"rough({template: none.out, strength: 1, points: 20, taper: 'none', randomize: true, clamp: false})"})
 
 
-let masterTL = gsap.timeline({repeat: -1}).pause()
+let masterTL = gsap.timeline({}).pause()
 
 words.forEach(word => {
-    let tl = gsap.timeline({repeat: 1, yoyo: true, repeatDelay: 1})
-    tl.to('.text', {duration: 1, text: word})
+    let tl = gsap.timeline()
+    tl.to('.text', {duration: 2, text: word})
     masterTL.add(tl)
 })
-
-gsap.to('p', {})
